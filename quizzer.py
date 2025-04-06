da793db50b8b48722551e5b1791ca05468ff72e0
jolsefu <jules.leo.reserva@gmail.com>
2025-04-06 08:23:54 +0800

    Fixed timer

--- quizzer.py
+++ quizzer.py
@@ -12,54 +12,63 @@ class QuizzerApp:
         self.question_index = 0
         self.questions = []
         self.enable_timer = False
-        self.score = 0
         self.root = None
+
+        self.selected_answer = None
+        self.correct_answer = None
+
+        self.score = 0
         self.score_label = None
         self.progress_bar = None
+
+        self.timer_label = None
+        self.time_left = 0
 
     def delete_all_widgets(self, **kwargs):
         for widget in self.root.winfo_children():
             if kwargs.get('all', False):
                 widget.destroy()
-            elif widget not in [self.progress_bar, self.score_label]:
+            elif widget not in [self.progress_bar, self.score_label, self.timer_label]:
                 widget.destroy()
 
+    def update_timer(self, *args):
+        if self.time_left > 0:
+            self.time_left -= 1
+            self.timer_label.config(text=f'Time left: {self.time_left} seconds')
+            self.root.after(1000, self.update_timer)
+        else:
+            self.check_answer()
+            self.timer_label.config(text=f"Time's up!")
+            self.time_left = 5
+
     def display_question(self):
-        def update_timer(timer_label, time_left):
-            if time_left > 0:
-                time_left -= 1
-                timer_label.config(text=f'Time left: {time_left} seconds')
-                self.root.after(1000, update_timer, timer_label, time_left)
-            else:
-                self.check_answer(selected_answer, correct_answer)
-                if timer_label.winfo_exists():
-                    timer_label.config(text=f"Time's up!")
-
         if self.enable_timer:
-            time_left = 5
-            timer_label = tk.Label(
-                self.root,
-                font=('Arial', 12),
-                text=f'Time left: {time_left}',
-                bg='white',
-                fg='red'
-            )
-            timer_label.pack(pady=10)
+            self.time_left = 5
 
-            update_timer(timer_label, time_left)
+            if self.timer_label is None:
+                self.timer_label = tk.Label(
+                    self.root,
+                    font=('Arial', 12),
+                    text=f'Time left: {self.time_left}',
+                    bg='white',
+                    fg='red'
+                )
+                self.timer_label.pack(pady=10)
+
+            self.update_timer()
 
         question_data = self.questions[self.question_index]
 
-        selected_answer = tk.StringVar()
+        self.selected_answer = tk.StringVar()
         question_type = len(question_data)
         question = question_data[0]
 
         if question_type == 6:
-            correct_answer = question_data[5]
+            self.correct_answer = question_data[5]
         elif question_type == 4:
-            correct_answer = question_data[3]
+            self.correct_answer = question_data[3]
         elif question_type == 2:
-            correct_answer = question_data[1]
+            self.correct_answer = question_data[1]
 
         tk.Label(self.root, text=question, bg='white', fg='black', wraplength=400, pady=10).pack()
 
@@ -70,7 +79,7 @@ class QuizzerApp:
                 tk.Radiobutton(
                     self.root,
                     text=option,
-                    variable=selected_answer,
+                    variable=self.selected_answer,
                     value=option,
                     bg='white'
                 ).pack(pady=2)
@@ -80,7 +89,7 @@ class QuizzerApp:
                 tk.Radiobutton(
                     self.root,
                     text=option,
-                    variable=selected_answer,
+                    variable=self.selected_answer,
                     value=option,
                     bg='white'
                 ).pack(pady=2)
@@ -88,21 +97,21 @@ class QuizzerApp:
         elif question_type == 2:
             entry = tk.Entry(
                 self.root,
-                textvariable=selected_answer
+                textvariable=self.selected_answer
             )
             entry.pack(pady=5)
 
         submit_button = tk.Button(
             self.root,
             text='Submit',
-            command=lambda: self.check_answer(selected_answer, correct_answer)
+            command=lambda: self.check_answer(self.selected_answer, self.correct_answer)
         )
         submit_button.pack(pady=10)
 
-    def check_answer(self, selected_answer, correct_answer):
+    def check_answer(self):
         self.delete_all_widgets()
 
-        if selected_answer.get().strip().lower() == correct_answer.strip().lower():
+        if self.selected_answer.get().strip().lower() == self.correct_answer.strip().lower():
             self.score += 1
             self.score_label.config(text=f'Score {self.score}', fg='green')
 
@@ -116,7 +125,7 @@ class QuizzerApp:
         else:
             tk.Label(
                 self.root,
-                text=f'Incorrect! The correct answer is: {correct_answer}',
+                text=f'Incorrect! The correct answer is: {self.correct_answer}',
                 fg='red',
                 bg='white',
                 font=('Arial', 12),
@@ -136,6 +145,7 @@ class QuizzerApp:
 
             progress = (self.question_index) / len(self.questions) * 100
             self.progress_bar['value'] = progress
+
             self.display_question()
         else:
             self.delete_all_widgets(all=True)
